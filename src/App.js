f61ca12f99c747e4e4a1cc8a588116d2ff686f55
webemnatt <104658987+webemnatt@users.noreply.github.com>
2023-04-03 18:02:26 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <>
+      <div class="redondo">rendondos</div>
+      <div class="arrow">triângulos</div>
+    </>
+  );
+}
