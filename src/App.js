9b3400ad2a8b3598d5c2da42af902fd8bfd12e5f
webemnatt <104658987+webemnatt@users.noreply.github.com>
2023-09-01 14:14:49 -0300

    pseudoelemento junto ao texto

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ export default function App() {
     <>
       <div class="redondo">rendondos</div>
       <div class="arrow">triângulos</div>
+      <div class="wrapper">
+        <div class="alive">
+          {/* <img class="label__icon" src="alive-icon.svg" alt="ícone ao vivo" />  */}
+          Aos vivos
+        </div>
+      </div>
     </>
   );
 }
